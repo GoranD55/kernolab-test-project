2c95750e16d29f616048502d147af4bc1c897899
Andrey Gorustowich <a.gorustovich@akdev.by>
2022-06-13 17:09:39 +0300

    Remove unused routes, change variable value in the env.example file

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::group(['prefix' => 'transactions', 'as' => '.transactions'], function() {
     Route::post('', [TransactionsController::class, 'store']);
     Route::post('/submit', [TransactionsController::class, 'submit']);
